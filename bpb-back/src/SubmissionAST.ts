b7f3f1ac20d5e23dc3fce8bea85450f9a0ab6322
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-11-08 22:00:03 -0500

    BPB-41 refactor: reworked basis for SubmissionAST based on chosen parser

--- bpb-back/src/SubmissionAST.ts
+++ bpb-back/src/SubmissionAST.ts
@@ -1,9 +1,24 @@
 import {IAnalysisResult} from './AnalysisResult';
+import { parse, ParseTree } from 'java-ast';
+import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor';
 
 export interface ISubmissionAST {
-    compare(otherSubmissionAST : ISubmissionAST) : IAnalysisResult
-    add(submissionAST : ISubmissionAST) : void
-    remove(submissionAST : ISubmissionAST) : void
-    getChild(submissionAST : ISubmissionAST) : void
-    getValue() : String
+    compare(otherSubmissionAST : ISubmissionAST) : IAnalysisResult;
+    accept(visitor : AbstractParseTreeVisitor<any>) : void;
 }
+
+export class SubmissionAST implements ISubmissionAST {
+    private ast : ParseTree;
+
+    constructor(fileContent : string) {
+        this.ast = parse(fileContent);
+    }
+    compare(otherSubmissionAST: ISubmissionAST): IAnalysisResult {
+        throw new Error('Method not implemented.');
+    }
+    accept(visitor: any): void {
+        throw new Error('Method not implemented.');
+    }
+}
+
+
