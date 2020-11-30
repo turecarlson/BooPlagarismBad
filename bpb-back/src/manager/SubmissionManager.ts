6c0c2c26145bb22d67f1305fa64ada1e722ab17e
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-11-30 15:22:53 -0500

    BPB-91 feat: implemented cache-ing for compareSubmission

--- bpb-back/src/manager/SubmissionManager.ts
+++ bpb-back/src/manager/SubmissionManager.ts
@@ -5,6 +5,7 @@ import fs from 'fs';
 import util from 'util';
 import SubmissionData from "../types/SubmissionData"
 import { AppConfig } from '../AppConfig';
+import e from 'express';
 const readFileContent = util.promisify(fs.readFile); //Promisify readfile  to allow use of Promise chaining
 
 /**
